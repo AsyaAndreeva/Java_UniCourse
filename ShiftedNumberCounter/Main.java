9e71afae7c1e8d8da8cb13bc7f6b7dca160aedcd
Asya Andreeva <117649237+AsyaAndreeva@users.noreply.github.com>
2024-03-03 15:49:28 +0200

    Added solution for Shifted Numbers task

---
+++ ShiftedNumberCounter/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        ShiftedNumbers number = new ShiftedNumbers(1000, 1200, 700);
+        System.out.println(number.numberOfShiftedNumbers());
+    }
+}
---
+++ ShiftedNumberCounter/ShiftedNumbers.java
@@ -0,0 +1,56 @@
+public class ShiftedNumbers {
+    private static int bottom;
+    private static int top;
+
+    private static int difference;
+
+
+    ShiftedNumbers() {
+    }
+
+    ShiftedNumbers(int bottom, int top, int difference) {
+        ShiftedNumbers.bottom = bottom;
+        ShiftedNumbers.top = top;
+        ShiftedNumbers.difference = difference;
+    }
+
+    int numberOfShiftedNumbers() {
+        int result = 0;
+        for (int number = bottom; number <= top; number++) {
+            String numberStr = Integer.toString(number);
+            String modifiedStr = numberStr.substring(1) + numberStr.charAt(0);
+            int modifiedNumber = Integer.parseInt(modifiedStr);
+            if (modifiedNumber - number > difference) {
+                result++;
+            }
+        }
+        return result;
+
+    }
+
+
+    public static int getBottom() {
+        return bottom;
+    }
+
+    public static void setBottom(int bottom) {
+        ShiftedNumbers.bottom = bottom;
+    }
+
+    public static int getTop() {
+        return top;
+    }
+
+    public static void setTop(int top) {
+        ShiftedNumbers.top = top;
+    }
+
+    public static int getDifference() {
+        return difference;
+    }
+
+    public static void setDifference(int difference) {
+        ShiftedNumbers.difference = difference;
+    }
+
+}
